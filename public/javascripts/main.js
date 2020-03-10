d43578d2e0995e155706be062fa01dc35d4e42de
Santiago Serrano <sd.serrano@uniandes.edu.co>
2020-03-09 19:48:06 -0500

    Final

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,33 @@
 let socket = io.connect("http://localhost:3000", { "forceNew": true });
 
+var bloqueo = false;
+
 socket.on("licitaciones", data => {
-    document.getElementById("ofertarbtn").disabled = true;
     render(data);
+});
+
+socket.on("licitaciones-aceptado", data => {
+    bloqueo = true;
+    document.getElementById("ofertarbtn").disabled = true;
 });
 
 function render(data) {
     let html = data.map((e, i) => {
-        return (`
-            <div>
-                <p>${e.razon} ${e.oferta}</>
-            </div>
-        `);
+        if (e.aceptada) {
+            bloqueo = true;
+            return (`
+                <div>
+                    <p>${e.razon} [<strong>Oferta aceptada. Valor: $${e.oferta}]</strong></p>
+                </div>
+            `);
+        } else {
+            return (`
+                <div>
+                    <p>${e.razon} [Oferta no aceptada]</>
+                </div>
+            `);
+        }
+
     }).join(" ");
     console.log("Renderizando licitaciones");
     document.getElementById("licitaciones").innerHTML = html;
@@ -23,13 +39,39 @@ function addLicitacion() {
         razon: document.getElementById("razonsocial").value,
         oferta: 0
     };
-    console.log("Emitting new licitacion");
-    socket.emit("new-licitacion", licitacion);
-    document.getElementById("nit").value = "";
-    document.getElementById("razonsocial").value = "";
+
+    console.log("Inscrito correctamente");
 
     document.getElementById("myBtn").disabled = true;
-    document.getElementById("ofertarbtn").disabled = false;
+    if(!bloqueo){
+        document.getElementById("ofertarbtn").disabled = false;
+    }
+    document.getElementById("nit").disabled = true;
+    document.getElementById("razonsocial").disabled = true;
 
     return false;
 }
+
+function addOferta() {
+    console.log("Ofertando");
+    let oferta = {
+        nit: document.getElementById("nit").value,
+        razon: document.getElementById("razonsocial").value,
+        oferta: 0,
+        aceptada: false
+    }
+    socket.emit("new-licitacion", oferta);
+
+    document.getElementById("ofertarbtn").disabled = true;
+    setTimeout(() => {
+        if(bloqueo){
+            document.getElementById("ofertarbtn").disabled = true;
+        } else {
+            document.getElementById("ofertarbtn").disabled = false;
+        }
+    }, 30000);
+
+    return false;
+}
+
+
--- socketApi.js
+++ socketApi.js
@@ -7,21 +7,37 @@ socketApi.io = io;
 const state = {
     licitaciones: [
         
-    ]
+    ],
+    mayor: 0,
+    aceptada: false
 }
 
 io.on('connection', function (socket) {
 
     io.sockets.emit('licitaciones',state.licitaciones);
+    if(state.aceptada){
+        socketApi.sendNotificationAceptado();
+    }
 
     socket.on("new-licitacion", data => {
-        console.log(data);
+        
         if(state.licitaciones.length<=0){
             data.oferta = 150000000;
         }
         else{
-            data.oferta = Math.random();
+            data.oferta = state.mayor + Math.floor(Math.random() * (10000000 - 5000000 + 1)) + 5000000;
         }
+        state.mayor = data.oferta;
+
+        var PB = (Math.random() * (0.8 - 0.3 )) + 0.3;
+        var PO = (Math.random() * (0.8 - 0.3 )) + 0.3;
+
+        if(PO>PB){
+            socketApi.sendNotificationAceptado();
+            data.aceptada = true;
+            state.aceptada = true;
+        }
+        
         const aux = state.licitaciones.concat(data);
         state.licitaciones = aux;
         socketApi.sendNotification();
@@ -30,6 +46,10 @@ io.on('connection', function (socket) {
 
 socketApi.sendNotification = () => {
     io.sockets.emit('licitaciones',state.licitaciones);
+}
+
+socketApi.sendNotificationAceptado = () => {
+    io.sockets.emit('licitaciones-aceptado',state.licitaciones);
 }
 
 module.exports = socketApi;
